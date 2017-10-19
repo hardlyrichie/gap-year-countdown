e8f7a0479a98a5663e77a8899f8cec665b940e0f
hardlyrichie <hardlyrichie@gmail.com>
2017-10-19 13:30:36 -0400

    Added Move In Date & Time Left CountDownTimer

--- app/src/main/java/com/richgao/gapyearcountdown/MainActivity.java
+++ app/src/main/java/com/richgao/gapyearcountdown/MainActivity.java
@@ -1,29 +1,75 @@
 package com.richgao.gapyearcountdown;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextClock;
 import android.widget.TextView;
 
 import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextClock currentTime;
     private TextView currentDate;
+    private TextView daysLeft, hoursLeft, minutesLeft, secondsLeft;
+    private long startTime;
+
+    private static final String TAG = "Gap Year Countdown";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        currentTime = (TextClock) findViewById(R.id.currentTime);
-        currentTime.setFormat12Hour("hh:mm:ss a");
-
         currentDate = (TextView) findViewById(R.id.currentDateTextView);
-        currentDate.setText(DateFormat.getDateInstance().format(new Date()));
+        currentDate.setText(DateFormat.getDateInstance().format(new Date())); // Gets Current Date in default format
 
+        currentTime = (TextClock) findViewById(R.id.currentTime);
+
+        daysLeft = (TextView) findViewById(R.id.daysLeftTextView);
+        hoursLeft = (TextView) findViewById(R.id.hoursLeftTextView);
+        minutesLeft = (TextView) findViewById(R.id.minutesLeftTextView);
+        secondsLeft = (TextView) findViewById(R.id.secondsLeftTextView);
+
+        String endTime = "25.8.2018, 00:00:00"; // Olin Move-in Day
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyy, HH:mm:ss");
+        formatter.setLenient(false); // Tells parsing to not be lenient (so don't interpret garbage)
+
+        Date endDate;
+        long miliseconds = 0;
+        try {
+            endDate = formatter.parse(endTime);
+            miliseconds = endDate.getTime(); // Gets the time of endDate in miliseconds
+        } catch (ParseException e) {
+            Log.e(TAG, "Failed to parse endTime" + e.getStackTrace());
+            finish();
+        }
+
+        startTime = System.currentTimeMillis(); // Placed as close as endDate.getTime() as possible
+
+        // Implement abstract class CountDownTime with endTime counting down every second
+        CountDownTimer countDown = new CountDownTimer(miliseconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Decrements startTime to simulate one second passing since previous onTick call
+                long diff = (millisUntilFinished - (--startTime)) / 1000;
+
+                daysLeft.setText(String.format("%d", diff / 86400));
+                hoursLeft.setText(String.format("%d", (diff % 86400) / 3600));
+                minutesLeft.setText(String.format("%d", ((diff % 86400) % 3600) / 60));
+                secondsLeft.setText(String.format("%d", ((diff % 86400) % 3600) % 60));
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
 
     }
 }
