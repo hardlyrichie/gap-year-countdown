91a9aa80e19c8c658bc25d5135da61eae2a944c0
hardlyrichie <hardlyrichie@gmail.com>
2017-10-19 11:23:31 -0400

    Added Current Time

--- app/src/main/java/com/richgao/gapyearcountdown/MainActivity.java
+++ app/src/main/java/com/richgao/gapyearcountdown/MainActivity.java
@@ -1,13 +1,29 @@
 package com.richgao.gapyearcountdown;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextClock currentTime;
+    private TextView currentDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentTime = (TextClock) findViewById(R.id.currentTime);
+        currentTime.setFormat12Hour("hh:mm:ss a");
+
+        currentDate = (TextView) findViewById(R.id.currentDateTextView);
+        currentDate.setText(DateFormat.getDateInstance().format(new Date()));
+
+
     }
 }
